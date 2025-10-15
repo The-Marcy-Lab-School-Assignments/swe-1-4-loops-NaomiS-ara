1d70fec1e254bf0220fb0d965124a003260d52b6
[NaomiS-ara] <[Naomispencer101@gmail.com]>
2025-10-14 22:08:06 -0400

    swe-1-4-loops-Naomi-1-10-COMPLETED

--- src/modify.js
+++ src/modify.js
@@ -1,18 +1,17 @@
 const whileToFor = () => {
-  let i = 0;
-  while (i < 5) {
+  for (let i = 0; i < 10; i++) {
     console.log(i);
-    i++;
   }
-};
+}
 
 const continueGuardClause = () => {
-  for (let i = 1; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
     // Add a guard clause to skip 2 and 3
-    console.log("Sure glad this isn't 2 or 3");
+    if (i === 2 || i === 3) continue;
+    //console.log("Sure glad this isn't 2 or 3");
     console.log(i);
   }
-};
+}
 
 module.exports = {
   whileToFor,
